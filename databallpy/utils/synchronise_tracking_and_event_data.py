89a5c498b5ad70e5d16a82240b1eba55b06d95ec
Max Basurto <maxabasurto@gmail.com>
2026-06-25 10:11:29 -0500

    reset to even weights

--- databallpy/utils/synchronise_tracking_and_event_data.py
+++ databallpy/utils/synchronise_tracking_and_event_data.py
@@ -672,7 +672,7 @@ def combine_cost_functions(
     if weights_dict is None:
         weights_dict = {
             "time_cost": 1.0,
-            "ball_event_dist_cost": 1.5,
+            "ball_event_dist_cost": 1.0,
             "ball_player_dist_cost": 1.0,
             "ball_acc_cost": 1.0,
             "player_ball_dist_inc_cost": 1.0,
